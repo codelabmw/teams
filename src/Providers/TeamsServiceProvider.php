51865f50b91c63aa050d7cdd77442028b3cd9279
Chikondi Kamwendo <kondie208@gmail.com>
2024-03-22 07:31:42 +0200

    Backward compatibility with laravel 10

--- src/Providers/TeamsServiceProvider.php
+++ src/Providers/TeamsServiceProvider.php
@@ -21,14 +21,15 @@ class TeamsServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // publish migrations
-        $this->publishesMigrations([
-            __DIR__ . '/../../database/migrations' => database_path('migrations')
-        ], static::GROUP);
-
-        // publish other files
+        // publish package files
         $this->publishes([
+            // config file
             __DIR__.'/../../config/teams.php' => config_path('teams.php'),
+
+            // migrations
+            __DIR__.'/../../database/migrations/2024_03_21_062826_create_teams_table.php' => database_path('migrations/2024_03_21_062826_create_teams_table.php'),
+            __DIR__.'/../../database/migrations/2024_03_21_094513_create_member_team_table.php' => database_path('migrations/2024_03_21_094513_create_member_team_table.php'),
+            __DIR__.'/../../database/migrations/2024_03_21_172901_create_resource_team_table.php' => database_path('migrations/2024_03_21_172901_create_resource_team_table.php'),
         ], static::GROUP);
     }
 }
